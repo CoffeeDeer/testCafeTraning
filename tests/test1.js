59c588f10a16051e5671d592c4eb26f432207d76
김현기 <11609005+rubi1541@users.noreply.github.com>
2020-07-19 23:27:10 +0900

    organized tests

--- tests/test1.js
+++ tests/test1.js
@@ -1,18 +1,48 @@
-import { Selector } from 'testcafe';
+import { Selector } from "testcafe";
 
-fixture `Getting Started`
-    .page `http://devexpress.github.io/testcafe/example`;
+fixture`Getting Started`.page`http://devexpress.github.io/testcafe/example`;
 
-test('My first test', async t => {
-
-    debugger;
+test("My first test", async (t) => {
+    //debugger;
 
     await t
-        .typeText('#developer-name', 'John Smith')
-        .click('#submit-button');
+        .typeText("#developer-name", "John Smith")
+        .click("#submit-button")
+        //change page
+        .expect(Selector("#article-header").innerText)
+        .eql("Thank you, John Smith!");
 
-    const articleHeader = await Selector('.result-content').find('h1');
+    const articleHeader = await Selector(".result-content").find("h1");
 
     // Obtain the text of the article header
     let headerText = await articleHeader.innerText;
 });
+
+fixture`My fixture`;
+
+test("Test1", async (t) => {
+    /* Test 1 Code */
+});
+
+test("Test2", async (t) => {
+    /* Test 2 Code */
+});
+
+fixture`MyFixture2`
+    .meta("fixtureID", "f-0001")
+    .meta({ author: "John", creationDate: "05/03/2018" });
+
+test.page`http://devexpress.github.io/testcafe/example`("MyTest", async (t) => {
+    /* ... */
+});
+
+//specificPage
+test
+    .meta("testID", "t-0005")
+    .meta({ severity: "critical", testedAPIVersion: "1.0" })
+    .page`http://devexpress.github.io/testcafe/blog/`(
+    "specificPage test",
+    async (t) => {
+        // Starts at http://devexpress.github.io/testcafe/blog/
+    }
+);
