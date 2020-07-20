e87250557ce8f69c1ba8c1c8ff8f5195c1bbc950
김현기 <11609005+rubi1541@users.noreply.github.com>
2020-07-20 23:52:32 +0900

    organized tests2

--- tests/test1.js
+++ tests/test1.js
@@ -4,7 +4,6 @@ fixture`Getting Started`.page`http://devexpress.github.io/testcafe/example`;
 
 test("My first test", async (t) => {
     //debugger;
-
     await t
         .typeText("#developer-name", "John Smith")
         .click("#submit-button")
@@ -46,3 +45,32 @@ test
         // Starts at http://devexpress.github.io/testcafe/blog/
     }
 );
+
+fixture`My fixture`
+    .page`http://example.com`
+    .beforeEach(async (t) => {
+        await t.useRole(admin).click("#open-management-console");
+    })
+    .afterEach(async (t) => {
+        await t.click("#delete-data");
+    });
+
+test.before(async (t) => {
+    await t.useRole(admin).click("#open-management-console");
+})("MyTest", async (t) => {
+    /* ... */
+}).after(async (t) => {
+    await t.click("#delete-data");
+});
+
+
+fixture.skip `Fixture 1`; // All tests in this fixture are skipped
+
+test('Fixture 1 - Test 1', () => {});
+test('Fixture 1 - Test 2', () => {});
+
+fixture `Fixture 2`;
+
+test('Fixture 2 - Test 1', () => {});
+test.skip('Fixture 2 - Test 2', () => {}); // This test is skipped
+test('Fixture 2 - Test 3', () => {});
